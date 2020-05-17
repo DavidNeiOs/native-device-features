e8355a8d11e3a12025edbb71f1839a3210b6bd37
DavidNeiOs <davidneios25@gmail.com>
2020-05-17 17:31:17 -0400

    initialize sqlite

--- App.tsx
+++ App.tsx
@@ -4,6 +4,15 @@ import { Provider } from 'react-redux';
 
 import { store } from './store';
 import { PlacesNavigator } from './navigation/PlacesNavigator';
+import { init } from './helpers/db';
+
+init()
+  .then(() => {
+    console.log('Initialize db');
+  })
+  .catch((error) => {
+    console.log('Initialize failed', error);
+  });
 
 export default function App() {
   return (
