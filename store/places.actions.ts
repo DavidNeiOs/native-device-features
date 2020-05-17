5f584d340451b76fec1a04e9509ecbd6c54860b2
DavidNeiOs <davidneios25@gmail.com>
2020-05-17 17:09:18 -0400

    add file system capability

--- store/places.actions.ts
+++ store/places.actions.ts
@@ -1,3 +1,8 @@
+import * as FileSystem from 'expo-file-system';
+import { ThunkAction } from 'redux-thunk';
+
+import { RootState } from './index';
+
 export const ADD_PLACE = 'ADD_PLACE';
 
 interface AddPlaceAction {
@@ -7,9 +12,27 @@ interface AddPlaceAction {
     image: string;
   };
 }
-
-export const addPlace = (title: string, image: string) => {
-  return { type: ADD_PLACE, placeData: { title, image } };
-};
-
 export type PlacesActions = AddPlaceAction;
+type ThunkResult<R> = ThunkAction<R, RootState, undefined, PlacesActions>;
+
+export const addPlace = (
+  title: string,
+  image: string
+): ThunkResult<void> => async (dispatch) => {
+  const fileName = image.split('/').pop();
+  const newPath = FileSystem.documentDirectory
+    ? FileSystem.documentDirectory + fileName
+    : '';
+
+  try {
+    await FileSystem.moveAsync({
+      from: image,
+      to: newPath,
+    });
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+
+  dispatch({ type: ADD_PLACE, placeData: { title, image: newPath } });
+};
